07eef4992786a07cbf282fc3e11989826113918d
Addin Munawwar <moonawar@protonmail.com>
2025-02-12 12:33:01 +0700

    chore: clean up code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,11 @@ import Enums.*;
 import Models.*;
 import Services.*;
 import com.microsoft.signalr.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Logger logger = LoggerFactory.getLogger(Main.class);
         BotService botService = new BotService();
         String token = System.getenv("Token");
         token = (token != null) ? token : UUID.randomUUID().toString();
